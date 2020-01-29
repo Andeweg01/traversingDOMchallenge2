c65e585b5e922fd31965e52b6a69200adb0cc798
Sascha Andeweg <sascha@designed.irish>
2020-01-29 21:43:40 +0000

    commit of a first try using slideDown in jQuery

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 //waits until page is ready
 $(document).ready(function() {
- 
 
-
-
-}); 
+    // select card_image and use slideDown on the paragraph when clicked
+    $("card-image").on("click", function(){
+        $("card_para").css("card_paravisible").slideDown(2000);
+    })
+});
